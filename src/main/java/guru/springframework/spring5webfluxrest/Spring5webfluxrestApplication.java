08d480b7583d779bf95cd01cc09a047a0a6f3fa7
Feng Bo <bobbyfeng@cse.com.sg>
2018-02-05 14:26:47 +0800

    initial commit

---
+++ src/main/java/guru/springframework/spring5webfluxrest/Spring5webfluxrestApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.spring5webfluxrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webfluxrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webfluxrestApplication.class, args);
+	}
+}
